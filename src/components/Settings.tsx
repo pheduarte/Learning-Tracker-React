63cd5ef5af37780ca4dbdcaea080585ac90416fb
Phelippe Duarte Ambrosio <14623@ait.nsw.edu.au>
2026-05-08 16:49:10 +1000

    feat: update Settings component for theme toggle and enhance styling for new-todo-form

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,16 +1,30 @@
 import { useTheme } from "../context/useTheme";
-import Button from "./ui/Button";
+// import Button from "./ui/Button";
 
 function Settings() {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <section className="settings">
-      <p>Settings coming soon!</p>
+      <p>Switch to {theme === "light" ? "dark" : "light"} mode </p>
 
-      <Button onClick={toggleTheme}>
+      {/* <Button onClick={toggleTheme}>
         Switch to {theme === "light" ? "dark" : "light"} mode
-      </Button>
+      </Button> */}
+
+      <div className="relative inline-block w-11 h-5">
+        <input
+          checked={theme === "dark"}
+          onChange={toggleTheme}
+          id="switch-component-blue"
+          type="checkbox"
+          className="peer appearance-none w-11 h-5 bg-slate-100 rounded-full checked:bg-blue-600 cursor-pointer transition-colors duration-300"
+        />
+        <label
+          htmlFor="switch-component-blue"
+          className="absolute top-0 left-0 w-5 h-5 bg-white rounded-full border border-slate-300 shadow-sm transition-transform duration-300 peer-checked:translate-x-6 peer-checked:border-blue-600 cursor-pointer"
+        ></label>
+      </div>
     </section>
   );
 }
